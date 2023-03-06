7a9d25f1032142e3c30b48dfcc4ad1eebcafdaf5
Sarthi Kalathiya <sarthikalathiya@gmail.com>
2023-03-06 11:10:05 +0530

    sign up

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -26,6 +26,6 @@ urlpatterns = [
     path('add-to-cart/<int:pk>', views.add_to_cart_view,name='add-to-cart'),
     path('cart', views.cart_view,name='cart'),
     path('remove-from-cart/<int:pk>', views.remove_from_cart_view,name='remove-from-cart'),
-    path('aboutus', views.aboutus_view),
-    path('customersignup', views.customer_signup_view)
+    path('customersignup', views.customer_signup_view),
+    path('aboutus', views.aboutus_view)
 ]
